3b15922187b88397fa9a3b26a8d0df12e0bf6612
Roberto Keane <robert96keane@gmail.com>
2019-07-28 21:20:03 +0700

    updating signin and creating new component custom button & form input

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
 import './sign-in.styles.scss';
 
 class SignIn extends React.Component {
@@ -27,16 +30,30 @@ class SignIn extends React.Component {
     render() {
         return (
             <div className='sign-in'>
-                <h2>I already have an account</h2>
+                <h2 className="title">I already have an account</h2>
                 <span>Sign in with your email and password</span>
 
                 <form onSubmit={this.handleSubmit}>
-                    <input name="email" type='email' value={this.state.email} onChange={this.handleChange} required />
-                    <label>Email</label>
-                    <input name="password" type='password' value={this.state.password} onChange={this.handleChange} required />
-                    <label>Password</label>
+                    <FormInput 
+                        name="email" 
+                        type='email' 
+                        value={this.state.email} 
+                        handleChange={this.handleChange} 
+                        label='email' 
+                        required 
+                    />
+                    
+                    <FormInput 
+                        name="password" 
+                        type='password' 
+                        value={this.state.password} 
+                        handleChange={this.handleChange} 
+                        label='password' 
+                        required 
+                    />
+                    
 
-                    <input type='submit' value='Submit Form' />
+                    <CustomButton type='submit'> SIGN IN </CustomButton>
                 </form>
             </div>
         )
